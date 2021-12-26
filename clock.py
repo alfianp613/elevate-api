64162c8ccc6649aaa62f54785a9cd1d14bb8df95
Muhammad Alfian Pratama <alfianp613@gmail.com>
2021-12-26 21:48:48 +0700

    Add task scheduler

---
+++ clock.py
@@ -0,0 +1,14 @@
+from apscheduler.schedulers.blocking import BlockingScheduler
+from sentiment import *
+from SVR import *
+
+sched = BlockingScheduler(timezone='Asia/Bangkok')
+koin = ['bitcoin','ethereum','binance coin','tether','solana',
+        'cardano','xrp','usd coin','polkadot','dogecoin']
+
+@sched.scheduled_job('cron', day_of_week='sun', hour=21, minute=55)
+def scheduled_job():
+    for i in koin:
+        forecast_SVR(i)
+
+sched.start()
