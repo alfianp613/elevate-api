504dc103bc90e2628756129bf83e74de094f0ac9
Muhammad Alfian Pratama <alfianp613@gmail.com>
2021-12-26 22:54:26 +0700

    Final API

--- clock.py
+++ clock.py
@@ -3,12 +3,39 @@ from sentiment import *
 from SVR import *
 
 sched = BlockingScheduler(timezone='Asia/Bangkok')
-koin = ['bitcoin','ethereum','binance coin','tether','solana',
-        'cardano','xrp','usd coin','polkadot','dogecoin']
 
-@sched.scheduled_job('cron', day_of_week='sun', hour=22, minute=45)
+
+@sched.scheduled_job('cron', day_of_week='mon-sun', hour=0)
 def scheduled_job():
-    for i in koin:
-        forecast_SVR(i)
+        koin = ['bitcoin','ethereum','binance coin','tether','solana','cardano','xrp','usd coin','polkadot','dogecoin']
+        for i in koin: 
+                forecast_SVR(i)
+
+@sched.scheduled_job('cron', day_of_week='mon-sun', hour=0, minute=5)
+def scheduled_job2():
+        koin = ['bitcoin','ethereum']
+        for i in koin: 
+                sentimen(i)
+@sched.scheduled_job('cron', day_of_week='mon-sun', hour=0, minute=10)
+def scheduled_job3():
+        koin = ['binance coin','tether']
+        for i in koin: 
+                sentimen(i)
+@sched.scheduled_job('cron', day_of_week='mon-sun', hour=0, minute=15)
+def scheduled_job4():
+        koin = ['solana','cardano']
+        for i in koin: 
+                sentimen(i)
+@sched.scheduled_job('cron', day_of_week='mon-sun', hour=0, minute=20)
+def scheduled_job5():
+        koin = ['xrp','usd coin']
+        for i in koin: 
+                sentimen(i)
+@sched.scheduled_job('cron', day_of_week='mon-sun', hour=0, minute=25)
+def scheduled_job6():
+        koin = ['polkadot','dogecoin']
+        for i in koin: 
+                sentimen(i)
+                
 
 sched.start()
